ecff4e10348b07a65507399fb1c8b6d6cd4cd236
Ed Johnson-Williams <git@johnsonwilliams.co.uk>
2024-11-02 23:30:45 +0000

    make changes for svelte 5

--- resources/js/ssr.js
+++ resources/js/ssr.js
@@ -1,5 +1,6 @@
 import { createInertiaApp } from "@inertiajs/svelte";
 import createServer from "@inertiajs/svelte/server";
+import { render } from 'svelte/server'
 
 createServer((page) =>
     createInertiaApp({
@@ -9,6 +10,9 @@ createServer((page) =>
                 eager: true,
             });
             return pages[`./Pages/${name}.svelte`];
+        },
+        setup({ App, props }) {
+            return render(App, { props })
         },
     })
 );
